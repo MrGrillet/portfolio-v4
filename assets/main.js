a965edc72c609c06e1cbe447174d4627060e72ca
Peter <pagrillet@gmail.com>
2018-03-12 12:06:38 +0000

    Starting to add real content to the template and feel out a design for the home page.

--- assets/main.js
+++ assets/main.js
@@ -26,7 +26,7 @@ var myImage = document.getElementById("slide-image");
 	setInterval(changeImage, 2000);
 
 
-
+// Manual Slider
   var slideIndex = 1;
   showDivs(slideIndex);
 
@@ -40,16 +40,13 @@ var myImage = document.getElementById("slide-image");
 
   function showDivs(n) {
     var i;
-    var x = document.getElementsByClassName("mySlides");
+    var x = document.getElementsByClassName("slides");
     var dots = document.getElementsByClassName("demo");
     if (n > x.length) {slideIndex = 1}
     if (n < 1) {slideIndex = x.length}
     for (i = 0; i < x.length; i++) {
        x[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-       dots[i].className = dots[i].className.replace(" w3-white", "");
-    }
+
     x[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " w3-white";
   }
